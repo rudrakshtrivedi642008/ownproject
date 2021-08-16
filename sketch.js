c270ecc06976d815e311c6301f1d811b69122df3
rudrakshtrivedi642008 <83581797+rudrakshtrivedi642008@users.noreply.github.com>
2021-08-16 13:52:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var backgroundImg;
+var player,pokemon;
+function prelode(){
+ backgroundImg=loadImage("IMAGE/training.jpg");
+}
+function setup (){
+    canvas = createCanvas(400,400);
+  var backgroundImg=(400,400);
+}
+function draw (){
+    background(0);
+backgroundImg.display();
+}
